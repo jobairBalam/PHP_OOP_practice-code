235581727bd9b82b96de1ba9188c216e1a65938e
jobairBalam <49426510+jobairBalam@users.noreply.github.com>
2019-11-13 11:47:32 +0600

    Create sapceship.php

---
+++ sapceship.php
@@ -0,0 +1,17 @@
+<?php
+   //integer comparison
+   print( 2 <=> 1);print("<br/>");   // x>y return  1
+   print( 1 <=> 1);print("<br/>");   // x=y return  0
+   print( 1 <=> 2);print("<br/>");   // x<y return -1
+   print("<br/>");
+   //float comparison
+   print( 2.5 <=> 1.5);print("<br/>");
+   print( 1.5 <=> 1.5);print("<br/>");
+   print( 1.5 <=> 2.5);print("<br/>");
+   print("<br/>");
+   //string comparison
+   print( "b" <=> "a");print("<br/>");
+   print( "a" <=> "a");print("<br/>");
+   print( "a" <=> "b");print("<br/>");
+   
+?>
